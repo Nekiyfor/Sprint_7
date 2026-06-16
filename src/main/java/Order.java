990ae54728be158cb7f4bf474cba5e49798ca602
n.fursov <n.fursov@vkteam.ru>
2026-06-16 18:54:46 +0300

    add class order

---
+++ src/main/java/Order.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Order {
+
+    private List<String> color;
+
+    public Order() {
+    }
+
+    public List<String> getColor() {
+        return color;
+    }
+
+    public void setColor(List<String> color) {
+        this.color = color;
+    }
+}
